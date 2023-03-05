56ca793bfbc48b55a4ee3c2eb59e094c8769836b
Radamel-09 <tiago9andre@gmail.com>
2023-03-05 21:16:33 +0000

    TPC3 done

--- TPC3/processo.py
+++ TPC3/processo.py
@@ -1,20 +1,22 @@
 from os.path import exists
 import re
 
-matches = 0
+matches = lines = 0
 frequency_dict = {}
 names_dict = {}
 surnames_dict = {}
+relacoes_dict = {}
+json_reg = []
 
 def frequency(line):
 
-    global matches,frequency_dict
+    global matches,lines,frequency_dict
 
-    x = re.match(r"(?P<pasta>\d+)::(?P<ano>[0-9]{4})-(?P<mês>[0-9]{2})-(?P<dia>[0-9]{2})::(?P<nome>[\w\s.]+)::([\w\s.]*)::([\w\s.]*)::", line)
+    x = re.match(r"(?P<pasta>\d+)::(?P<ano>[0-9]{4})-(?P<mes>[0-9]{2})-(?P<dia>[0-9]{2})::(?P<nome>[\w\s.]+)::(?P<pai>.*)::(?P<mae>.*)::(?P<extras>.*)::", line)
 
     if(x):
         #frequência por ano
-        year = int(x.group(2))
+        year = int(x.group('ano'))
         if year in frequency_dict:
             frequency_dict[year] += 1
         else:
@@ -25,8 +27,19 @@ def frequency(line):
         add_top_names(tuple)
         add_top_surnames(tuple)
         
+        #relações
+        if(x.group('extras')):
+            get_relacoes(line)
+
+        #json 
+        if(lines<20):
+            line_dict = {"pasta":x.group('pasta'),"data":x.group('ano') + "-" + x.group('mes') + "-" + x.group('dia'),"nome":x.group('nome'),"pai":x.group('pai'),"mae":x.group('mae'),"extras":x.group('extras')}
+            json_reg.append(line_dict)
+            lines += 1
+
         #37880
         matches += 1
+
 
 def show_frequency():
     sorted_dict = {k: frequency_dict[k] for k in sorted(frequency_dict)}
@@ -104,7 +117,7 @@ def top_5_names_sec():
 
 def top_5_surnames_sec():
 
-    for key, name_list in names_dict.items():
+    for key, name_list in surnames_dict.items():
         surnames_dict[key] = name_list[:5]
 
     sorted_dict = {k: surnames_dict[k] for k in sorted(surnames_dict)}
@@ -114,10 +127,38 @@ def top_5_surnames_sec():
         for name, freq in surname_list:
             print("  {} ({})".format(name, freq))
 
+def get_relacoes(line):
+
+    global relacoes_dict
+
+    matches = re.findall(r"(?:[A-Z][a-z]+),([A-Z][\w\s]+)\.",line)
+
+    if matches:
+        for match in matches:
+            if match in relacoes_dict:
+                relacoes_dict[match] += 1
+
+            else:
+                relacoes_dict[match] = 1
+        
+
+def show_relacoes():
+    sorted_dict = dict(sorted(relacoes_dict.items(), key=lambda x: x[1], reverse=True))
+    print(sorted_dict)
+
+def convert_json():
+    for reg in json_reg:
+        new_reg = str(reg).replace("'", '"')
+        print(new_reg)
+        
+
 def main():
 
     #path = input("Qual o caminho para o ficheiro?")
     path = "processos.txt"
+
+    lines = 0
+    twenty_lines = []
 
     if(not exists(path)):
         print("Ficheiro não existe.")
@@ -132,5 +173,5 @@ def main():
             for line in content.splitlines():
                 frequency(line)
 
-    print(matches)
+    #print(matches)
   
--- TPC3/run.py
+++ TPC3/run.py
@@ -1,10 +1,12 @@
-from os.path import exists
+import os
+import sys
 from processo import *
+import time
 
 def menu():
 
     #path = input("Qual o caminho para o ficheiro?")
-    path = "processos copy.txt"
+    path = "processos.txt"
 
     if(not exists(path)):
         print("Ficheiro não existe.")
@@ -12,25 +14,45 @@ def menu():
     
     else:
 
+        print("\nLOADING...\n")
+
+        #start_time = time.time()
+
         main()
 
-        print("\nMENU\n")
-        print("1: Mostrar a frequência de processos por ano.")
-        print("2: Mostrar a frequência de nomes próprios.")
-        print("3: Mostrar a frequência de apelidos.")
-        print("4: Mostrar a frequência dos vários tipos de relação.")
-        print("5: Converter os 20 primeiros registos num novo ficheiro de output mas em formato Json.")
-        print("\n")
+        #end_time = time.time()
 
-        opt = input("Selecione uma opção:")
-        print("\n")
+        #execution_time = end_time - start_time
+        #print("Execution time:", execution_time, "seconds")
+        
+        while(True):
+            
+            clear_screen()
+            print("\nMENU\n")
+            print("1: Mostrar a frequência de processos por ano.")
+            print("2: Mostrar a frequência de nomes próprios.")
+            print("3: Mostrar a frequência de apelidos.")
+            print("4: Mostrar a frequência dos vários tipos de relação.")
+            print("5: Converter os 20 primeiros registos num novo ficheiro de output mas em formato Json.")
+            print("6: Sair")
+            print("\n")
 
-        handler = options.get(opt)
-        if handler:
-            handler()
-        else:
-            print("Opção inválida")
+            opt = input("Selecione uma opção:")
+            print("\n")
 
+            handler = options.get(opt)
+            if handler:
+                handler()
+                input("\nPressione Enter para continuar...")
+            else:
+                print("Opção inválida")
+                input("\nPressione Enter para continuar...")
+
+def clear_screen():
+    if os.name == 'nt': # for Windows
+        os.system('cls')
+    else: # for Unix/Linux/MacOS
+        os.system('clear')
 
 def handle_option1():
     show_frequency()
@@ -42,10 +64,13 @@ def handle_option3():
     top_5_surnames_sec()
 
 def handle_option4():
-    print("not done")
+    show_relacoes()
 
 def handle_option5():
-    print("not done")
+    convert_json()
+
+def handle_option6():
+    sys.exit(0)
 
 options = {
     "1": handle_option1,
@@ -53,6 +78,7 @@ options = {
     "3": handle_option3,
     "4": handle_option4,
     "5": handle_option5,
+    "6": handle_option6,
 }
 
 menu()
